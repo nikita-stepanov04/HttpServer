89913785c6934e2d8918158e1d995a451e739049
nikita-stepanov04 <nikita.stepanov556789@gmail.com>
2025-01-03 11:45:40 +0200

    BugFix

--- DispatcherToolKit/ServersRepository.cs
+++ DispatcherToolKit/ServersRepository.cs
@@ -2,17 +2,37 @@
 {
     public static class ServersRepository
     {
-        private static Random _random = new Random();
+        private static readonly Random _random = new Random();
+        private static readonly object _lock = new object();
 
-        private static readonly List<int> _servers = new();
+        private static readonly List<int> _servers = new List<int>();
 
-        public static void AddServer(int server) => _servers.Add(server);
+        public static void AddServer(int server)
+        {
+            lock (_lock)
+            {
+                _servers.Add(server);
+            }
+        }
 
-        public static void RemoveServer(int server) => _servers.Remove(server);
+        public static void RemoveServer(int server)
+        {
+            lock (_lock)
+            {
+                _servers.Remove(server);
+            }
+        }
 
-        public static int? GetServer(int? requesterPort) => _servers
-            .Where(port => port != requesterPort)
-            .GetRandom();
+        public static int? GetServer(int? requesterPort)
+        {
+            lock (_lock)
+            {
+                var availableServers = _servers
+                    .Where(port => port != requesterPort)
+                    .ToList();
+                return availableServers.GetRandom();
+            }
+        }
 
         private static int? GetRandom(this IEnumerable<int> servers)
         {
